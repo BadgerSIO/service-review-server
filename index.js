4f492e1231a05200c9976722927f407fa689bfa1
BadgerSIO <saadibneomar@gmail.com>
2022-11-08 00:18:31 +0600

    mongodb connected and dotenv configured

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
+require("dotenv").config();
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -14,3 +15,18 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`precision law server is running on port: ${port}`);
 });
+
+const uri = `mongodb+srv://${process.env.PLAW_DBUSER}:${process.env.PLAW_DBPASS}@cluster0.gqpfnmn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const servicesCollection = client.db("precisionLaw").collection("sevices");
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
